15c4a2572a1214930861151e68381f89975828b1
Pablo LM <pabloplm@gmail.com>
2016-11-05 13:22:35 +0100

    Added transitions

--- src/components/PostsIndex.js
+++ src/components/PostsIndex.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { fetchPosts } from '../actions/index';
+import { fetchPosts, deletePost } from '../actions/index';
 import { Link } from 'react-router';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
 class PostsIndex extends React.Component {
     constructor(props) {
         super(props);
+        this.onRemovePost = this.onRemovePost.bind(this);
     }
 
     componentWillMount() {
@@ -14,22 +16,38 @@ class PostsIndex extends React.Component {
         this.props.fetchPosts();
     }
 
+    onRemovePost(id){
+        this.props.deletePost(id);
+    }
+
     renderPosts(){
         return this.props.posts.map((post) => {
             return (
-                <li className="list-group-item" key={post.id}>
-                    <Link to={ "posts/"+ post.id }>
-                        <span className="pull-xs-right">
-                            {post.categories}
-                        </span>
-                        <strong>{post.title}</strong>
-                    </Link>
-                </li>
+                    <li className="list-group-item" key={post.id}>
+                        <div className="flex-container">
+                            <Link to={ "posts/"+ post.id } className="flex-li-link flex-container">
+                                <div className="flex-li-title"><strong>{post.title}</strong></div>
+                                <div className="flex-li-category">
+                                    {post.categories}
+                                </div>
+                            </Link>
+                            <button
+                                className="btn btn-danger flex-li-button"
+                                onClick={() => this.onRemovePost(post.id)}
+                            >x</button>
+                        </div>
+                    </li>
             );
         })
     }
 
     render() {
+        const transitionOptions = {
+            transitionName: "fade",
+            transitionEnterTimeout: 500,
+            transitionLeaveTimeout: 500
+        };
+
         return (
             <div>
                 <div className="text-xs-right">
@@ -39,7 +57,9 @@ class PostsIndex extends React.Component {
                 </div>
                 <h3>Posts</h3>
                 <ul className="list-group">
-                    { this.renderPosts() }
+                    <ReactCSSTransitionGroup {...transitionOptions}>
+                        { this.renderPosts() }
+                    </ReactCSSTransitionGroup>
                 </ul>
             </div>
         );
@@ -51,7 +71,7 @@ function mapStateToProps(state) {
 }
 
 function mapDispatchToProps(dispath){
-    return bindActionCreators({ fetchPosts }, dispath);
+    return bindActionCreators({ fetchPosts, deletePost }, dispath);
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(PostsIndex);
--- src/reducers/ReducerPosts.js
+++ src/reducers/ReducerPosts.js
@@ -1,16 +1,21 @@
 /**
  * Created by pablolm on 1/11/16.
  */
-import { FETCH_POSTS, GET_POST } from '../actions/index';
+import _ from 'lodash';
+import { FETCH_POSTS, GET_POST, DELETE_POST } from '../actions/index';
 
 const INITIAL_STATE = { all:[], post: null };
 
 export default function(state = INITIAL_STATE, action){
     switch(action.type){
-        case GET_POST:
-            return { ...state, post: action.payload.data };
         case FETCH_POSTS:
             return { ...state, all: action.payload.data };
+        case GET_POST:
+            return { ...state, post: action.payload.data };
+        case DELETE_POST:
+            return {...state, all: _.filter(state.all, function(item){
+                return item.id != action.payload.data.id;
+            })};
         default:
             return state;
     }
